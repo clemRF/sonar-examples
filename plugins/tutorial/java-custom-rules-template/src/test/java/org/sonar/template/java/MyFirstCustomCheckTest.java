24027f92b128bd00d89abd5423a474bab5519781
clemRF <clemence.rigolet@airbus.com>
2016-08-10 16:34:06 +0200

    Create MyFirstCustomCheckTest.java

---
+++ plugins/tutorial/java-custom-rules-template/src/test/java/org/sonar/template/java/MyFirstCustomCheckTest.java
@@ -0,0 +1,24 @@
+
+package org.sonar.template.java.checks;
+
+ 
+
+import org.junit.Test;
+
+ 
+
+public class MyFirstCustomCheckTest {
+
+ 
+
+  @Test
+
+  public void test() {
+
+  }
+
+ 
+
+}
+
+ 
